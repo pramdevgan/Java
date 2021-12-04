5dbdc5cf862ccd6054454bdd186daac560408a9c
Pramatma Vishwakarma <vpramatma@gmail.com>
2021-12-04 14:24:55 +0530

    commit

---
+++ By Codio/Course 1/Week 1/Exercise1.java
@@ -0,0 +1,15 @@
+public class Exercise1 {
+    public static void main(String args[]) {
+      
+      boolean test = Boolean.parseBoolean(args[0]);
+      
+      //add code below this line
+      Boolean my_boolean = true;
+      System.out.println(my_boolean);
+      my_boolean = test;
+      System.out.println(my_boolean);
+  
+      
+      //add code above this line
+    }
+  }
---
+++ By Codio/Course 1/Week 1/Exercise2.java
@@ -0,0 +1,13 @@
+public class Exercise2 {
+    public static void main(String[] args){
+        int first_number = Integer.parseInt(args[0]);
+        int second_number = Integer.parseInt(args[1]);
+        int third_number = Integer.parseInt(args[2]);
+        int my_int = first_number;
+        System.out.println(my_int);
+        my_int = second_number;
+        System.out.println(my_int);
+        my_int = third_number;
+        System.out.println(my_int);
+    }
+}
